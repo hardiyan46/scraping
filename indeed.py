8f46201d0d144ebd06b33449ed9b1aac85cd9393
Hardiyan Purnama <hardiyan.purnama.hp@gmail.com>
2022-04-29 22:05:00 +0700

    penambahan function

---
+++ indeed.py
@@ -0,0 +1,64 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://id.indeed.com/jobs'
+params = {
+    'q' : 'python developer',
+    'l' : 'jakarta',
+    'vjk' : 'b8bc9c3e08bc8ae3'
+}
+headers = {'User Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0'}
+res = requests.get(url, params=params, headers=headers)
+#print(res.headers)
+#Scraping step
+#soup = BeautifulSoup(res.text,'html.parser')
+#print(soup.prettify())
+
+def get_total_pages():
+    params = {
+        'q': 'python developer',
+        'l': 'jakarta',
+        'vjk': 'b8bc9c3e08bc8ae3'
+    }
+
+    res = requests.get(url, params=params, headers=headers)
+    try:
+        os.mkdir('temp')
+    except FileExistsError:
+        pass
+    with open('temp/res.html', "w", encoding="utf-8") as outfile:
+        outfile.write(res.text)
+        outfile.close()
+    total_pages = []
+    #Scarping step
+    soup = BeautifulSoup(res.text, 'html.parser')
+    pagination = soup.find('ul', 'pagination-list')
+    pages = pagination.find_all('li')
+    for page in pages:
+        total_pages.append(page.text)
+    total = int(max(total_pages))
+    return total
+
+
+
+def get_items():
+    params = {
+        'q': 'python developer',
+        'l': 'jakarta',
+        'vjk': 'b8bc9c3e08bc8ae3'
+    }
+    res = requests.get(url, params=params, headers=headers)
+    with open('temp/res.html', "w", encoding="utf-8") as outfile:
+        outfile.write(res.text)
+        outfile.close()
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+#Scraping proses
+    contents = soup.find_all('table', '')
+
+
+
+if __name__ == '__main__':
+    get_total_pages()
+
